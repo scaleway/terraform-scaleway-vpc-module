a2532eda4902e2077e9b3a2b1bf2d7e7d2625ba5
jaime Bernabe <6184069+Monitob@users.noreply.github.com>
2023-04-25 10:32:21 +0200

    fix: variables on one file

--- private_network_variables.tf
+++
@@ -1,8 +0,0 @@
-################################################################################
-# PRIVATE NETWORK
-################################################################################
-variable "private_network_name" {
-  type        = string
-  default     = ""
-  description = "Name to be used on private network resource as identifier"
-}
