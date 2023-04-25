c90a1de5db9f7b54d6105714df319da0ef5a74d4
jaime Bernabe <6184069+Monitob@users.noreply.github.com>
2023-04-25 16:08:39 +0200

    fix: add required version

--- examples/simple/versions.tf
+++ examples/simple/versions.tf
@@ -1,7 +1,8 @@
 terraform {
   required_providers {
     scaleway = {
-      source = "scaleway/scaleway"
+      source  = "scaleway/scaleway"
+      version = ">= 2.17"
     }
   }
   required_version = ">= 0.13"
